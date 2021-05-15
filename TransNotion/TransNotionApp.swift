4090783746ca19343cbd89fccda4f95d4d6deac0
bannzai <kingkong999yhirose@gmail.com>
2021-05-15 16:11:44 +0900

    :sparkels: initial commit

---
+++ TransNotion/TransNotionApp.swift
@@ -0,0 +1,17 @@
+//
+//  TransNotionApp.swift
+//  TransNotion
+//
+//  Created by Yudai.Hirose on 2021/05/15.
+//
+
+import SwiftUI
+
+@main
+struct TransNotionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
