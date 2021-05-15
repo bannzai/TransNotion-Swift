dfc035cf290f76aaa9a19257baf6e4066537f1d6
bannzai <kingkong999yhirose@gmail.com>
2021-05-15 21:41:14 +0900

    :sparkles: add style directory

---
+++ TransNotion/Style/Color+Extension.swift
@@ -0,0 +1,12 @@
+//
+//  Color+Extension.swift
+//  TransNotion
+//
+//  Created by Yudai.Hirose on 2021/05/15.
+//
+
+import SwiftUI
+
+extension SwiftUI.Color {
+    static let background: Color = Color(red: 140 / 255, green: 140 / 255, blue: 140 / 255)
+}
