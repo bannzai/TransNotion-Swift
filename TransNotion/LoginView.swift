5fbf865fbbfae83bcce2d41ac15ddb779065533d
bannzai <kingkong999yhirose@gmail.com>
2021-05-15 21:41:34 +0900

    :sparkels: WIP create LoginView

--- TransNotion/LoginView.swift
+++ TransNotion/LoginView.swift
@@ -9,7 +9,22 @@ import SwiftUI
 
 struct LoginView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Color.background
+
+            VStack {
+                Button(action: {
+                    
+                }, label: {
+                    HStack {
+                        Image("notion_icon")
+                            .resizable()
+                        Text("Login with Notion")
+                    }
+                    .frame()
+                })
+            }
+        }
     }
 }
 
