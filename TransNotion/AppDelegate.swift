2453f218d4dac0a7f0959a98bf5b4a8a3dccc2f7
bannzai <kingkong999yhirose@gmail.com>
2021-05-15 21:41:25 +0900

    :recycle: avoid preview error

--- TransNotion/AppDelegate.swift
+++ TransNotion/AppDelegate.swift
@@ -8,9 +8,13 @@
 import UIKit
 import Firebase
 
+let isPreview = ProcessInfo.processInfo.environment["XCODE_RUNNING_FOR_PREVIEWS"] == "1"
+
 final class AppDelegate: NSObject, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        setupFirebase()
+        if !isPreview {
+            setupFirebase()
+        }
         return true
     }
 }
