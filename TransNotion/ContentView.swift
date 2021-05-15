7e0d3d8463d576321daec0b24b31c9c414da5528
bannzai <kingkong999yhirose@gmail.com>
2021-05-15 21:23:52 +0900

    :recycle: Fix adding ContentView

--- TransNotion/ContentView.swift
+++ TransNotion/ContentView.swift
@@ -5,4 +5,16 @@
 //  Created by Yudai.Hirose on 2021/05/15.
 //
 
-import Foundation
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
