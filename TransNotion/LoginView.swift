77f134d62115e1e8ac0fad755059f64a189e9460
bannzai <kingkong999yhirose@gmail.com>
2021-05-15 21:23:30 +0900

    :sparkels: add LoginView

---
+++ TransNotion/LoginView.swift
@@ -0,0 +1,20 @@
+//
+//  LoginView.swift
+//  TransNotion
+//
+//  Created by Yudai.Hirose on 2021/05/15.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LoginView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoginView()
+    }
+}
