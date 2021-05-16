95c6b217dc98d30c1a50fa363e519ac4d4dd8932
bannzai <kingkong999yhirose@gmail.com>
2021-05-17 07:01:40 +0900

    :sparkels: WIP add pages

---
+++ TransNotion/Domain/Pages/PagesView.swift
@@ -0,0 +1,28 @@
+//
+//  PagesView.swift
+//  TransNotion
+//
+//  Created by Yudai.Hirose on 2021/05/17.
+//
+
+import SwiftUI
+import Combine
+import notion
+
+struct PagesView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+extension PagesView {
+    final class ViewModel: ObservableObject {
+        
+    }
+}
+
+struct PagesView_Previews: PreviewProvider {
+    static var previews: some View {
+        PagesView()
+    }
+}
